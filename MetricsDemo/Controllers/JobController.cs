a74dbbf78f2ee2a85ce6c621d1a593db1da28b4c
SanSYS <i.sansys@gmail.com>
2019-01-20 14:17:01 +0500

    Добавлен кронджоб

---
+++ MetricsDemo/Controllers/JobController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        [HttpGet("run/{name}")]
+        public async Task<string> Run(string name)
+        {
+            await Console.Out.WriteLineAsync($"Run job [{name}]");
+
+            return $"Job [{name}] runned!";
+        }
+    }
+}
